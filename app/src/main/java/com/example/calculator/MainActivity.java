7123f33388470aec23702457a6aca236a5b0f38e
Nour E-Din <nourmanmatrix@gmail.com>
2019-11-05 15:42:54 +0200

    fixed some issues and improved performacne

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDot, btnc, btndel,btnpls, btnmns, btndiv, btnmul, btnzeros, btnmnspls, btneql;
     TextView tvmain, tvsmall;
 
-    boolean isdotadded, displayed, canchangesign;
+    boolean isdotadded, displayed, canchangesign ,zerodivision ;
     int MAX_LENGTH;
-    double sum = 0, lastEnteredNumber = 0;
+    double sum = 0, lastEnteredNumber;
     char nextop = '+';
 
     @Override
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         isdotadded = false;
         displayed = false;
         canchangesign = true;
+        zerodivision = false;
+        lastEnteredNumber = 0;
 
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
@@ -61,6 +63,13 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener numbersListner = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (zerodivision){
+                    cleartext();
+                    tvsmall.setText("");
+                    sum = 0;
+                    nextop = '+';
+                    zerodivision = false;
+                }
                 if (displayed)
                     cleartext();
                 canchangesign = false;
@@ -98,12 +107,19 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener actionListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (zerodivision) {
+                    Toast.makeText(getApplicationContext(), "Please Clear Or write numbers", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 String btnstr = ((Button) view).getText().toString();
                 String mainstr = tvmain.getText().toString();
 
                 if (mainstr.length() != 0) {
-                    if (!canchangesign)
+                    if (!canchangesign) {
+                        Log.d("piv", "onClick: before calc");
                         calc();
+                        Log.d("piv", "onClick after calc");
+                    }
                     switch (btnstr) {
                         case "+":
                             nextop = '+';
@@ -121,12 +137,15 @@ public class MainActivity extends AppCompatActivity {
                             nextop = '/';
                             break;
                     }
+                    char lastchar;
                     String smalltemp = tvsmall.getText().toString();
-                    char lastchar = smalltemp.charAt(smalltemp.length()-1);
-                    if (lastchar == '/' || lastchar == '*' || lastchar == '-' || lastchar == '+')
-                        tvsmall.setText(smalltemp.substring(0,smalltemp.length()-1) + nextop);
-                    else
-                        tvsmall.setText(smalltemp+""+nextop);
+                    if (smalltemp.length() > 0) {
+                        lastchar = smalltemp.charAt(smalltemp.length() - 1);
+                        if (lastchar == '/' || lastchar == '*' || lastchar == '-' || lastchar == '+')
+                            tvsmall.setText(smalltemp.substring(0, smalltemp.length() - 1) + nextop);
+                        else
+                            tvsmall.setText(tvmain.getText().toString() + "" + nextop);
+                    }
                 }else
                     Toast.makeText(getApplicationContext(),"Empty", Toast.LENGTH_SHORT);
             }
@@ -145,6 +164,7 @@ public class MainActivity extends AppCompatActivity {
                     mainstr = "-" + mainstr;
                 else
                     mainstr = mainstr.substring(1);
+                lastEnteredNumber = Double.parseDouble(mainstr);
                 tvmain.setText(mainstr);
             }
         });
@@ -153,11 +173,25 @@ public class MainActivity extends AppCompatActivity {
         btneql.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (displayed)
-                    calc2();
-                else if (tvmain.getText().toString().length() > 0) {
-                    calc();
-                    tvsmall.setText(tvsmall.getText().toString() + "" + nextop);
+                if (!zerodivision) {
+                    if (displayed) {
+                        calc2();
+                        tvsmall.setText(tvmain.getText().toString() + "" + nextop);
+                    }
+                    else if (tvmain.getText().toString().length() > 0) {
+                        calc();
+                        if (!zerodivision)
+                            tvsmall.setText(tvmain.getText().toString() + "" + nextop);
+                    }
+                    else {
+                        tvmain.setText(tvsmall.getText().toString().substring(0, tvsmall.getText().toString().length()-1));
+                    }
+                }else{
+                    cleartext();
+                    tvsmall.setText("");
+                    sum = 0;
+                    nextop = '+';
+                    zerodivision = false;
                 }
             }
         });
@@ -195,6 +229,8 @@ public class MainActivity extends AppCompatActivity {
                 cleartext();
                 tvsmall.setText("");
                 sum = 0;
+                nextop = '+';
+                zerodivision = false;
             }
         });
 
@@ -204,6 +240,7 @@ public class MainActivity extends AppCompatActivity {
                 if (displayed){
                     tvmain.setText("");
                     isdotadded = false;
+                    displayed = false;
                     return;
                 }
                 String s = tvmain.getText().toString();
@@ -268,11 +305,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case '/':
                 if (value == 0) {
-//                    Toast.makeText(getApplicationContext(), "Can't divide by zero", Toast.LENGTH_SHORT);
-//                    Log.d("piv", "calc: can't divide by zero");
+                    Log.d("piv", "calc1: can't divide by zero");
                     tvmain.setText("Can't divide by zero");
+                    zerodivision = true;
                     displayed = true;
                     canchangesign = true;
+                    tvsmall.setText("");
                     return;
                 }
                 else
@@ -307,6 +345,8 @@ public class MainActivity extends AppCompatActivity {
                     tvmain.setText("Can't divide by zero");
                     displayed = true;
                     canchangesign = true;
+                    zerodivision = true;
+                    tvsmall.setText("");
                     return;
                 }
                 else
